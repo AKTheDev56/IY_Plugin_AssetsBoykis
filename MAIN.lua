5e7a155d328b505cefeb8cbee78829e5b418c8e3
Nachos <avankimbleton270@gmail.com>
2025-03-31 22:34:47 -0400

    Create MAIN.lua

---
+++ MAIN.lua
@@ -0,0 +1,16 @@
+local Plugin = {
+    ["PluginName"] = "boykisserjumpscare",
+    ["PluginDescription"] = "Boykisser-jumpscares you",
+    ["Commands"] = {
+        ["COMMANDNAME"] = {
+            ["ListName"] = "boykisserjumpscare",
+            ["Description"] = "Jumpscares you with boykisser (ooh~ you like boys~ your a boy kisser~ haha ik i should kms)",
+            ["Aliases"] = {"boykiss","boykisser","boykisserjump","jumpkisserboy","jumpscarekisserboy"},
+            ["Function"] = function(args,speaker)
+              yadayada 
+            end
+        }
+     }
+}
+
+return Plugin
